ee61c8a574437d3bfe65fa7f220d34c05e3dcbc4
Kyu Woong Lee <kwooler@gmail.com>
2026-03-26 12:54:26 -0700

    Add full solution main_read.cpp

--- main_read.cpp
+++ main_read.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+    int M;
+    double score1, score2, sum, avg;
+    string stuName;
+    ifstream ifs;
+
+    // TODO: open "students.txt" for reading
+    ifs.open("students.txt");
+
+    // TODO: check if file opened successfully; print error and exit if not
+    if (!ifs.is_open()) {
+        cout << "Error: cannot open students.txt for reading" << endl;
+        exit(0);
+    }
+
+    // TODO: read M from ifs, print "Total M students"
+    ifs >> M;
+    cout << "Total " << M << " students" << endl;
+
+    for (int i = 0; i < M; i++)
+    {
+        // TODO: read stuName, score1, score2 from ifs
+        ifs >> stuName >> score1 >> score2;
+        // TODO: compute sum = score1 + score2, avg = sum / 2.0
+        sum = score1 + score2;
+        avg = sum / 2.0;
+        // TODO: print Student Name, score1, score2, Sum, Avg
+        cout << "Student Name: " << stuName
+             << "  score1: " << score1
+             << "  score2: " << score2
+             << "  Sum: " << sum
+             << "  Avg: " << avg << endl;
+    }
+    ifs.close();
+    return 0;
 }
